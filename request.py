d61bb427b776c565a7460a9e18cf8d6ea340996e
Timidvb <38565589+Timidvb@users.noreply.github.com>
2021-08-17 20:09:53 +0800

    Update request.py

--- request.py
+++ request.py
@@ -2,14 +2,12 @@ import requests
 
 link = open("link.txt", "r")
 
-url = "https://pptp.cloud/sub/eff3ccea-d40c-4861-92b8-c0e635ea7b86.html"
+link.readline().replace("\n", "")
 r = requests.get(url)
 
 print(url, r.text)
 
-full_page = """
-<html><head></head><body>%s</body></html>
-"""%(r.text)
+full_page = "%s"%(r.text)
 
 with open('sub.html', 'w') as f:
     f.write(full_page)
